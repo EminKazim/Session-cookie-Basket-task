23fab010cd4fc1d13ad9760ba255c916134cb78b
Emin <eminjk@code.edu.az>
2022-03-14 17:46:11 -0700

    hometask

---
+++ LessonMigrations/LessonMigrations/Areas/AdminArea/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using LessonMigrations.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMigrations.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.Where(m => !m.İsDeleted).ToListAsync();
+            return View(categories);
+        }
+        public IActionResult Detail(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(m => m.Id == id);
+            return View(category);
+        }
+        public IActionResult Edit (int id)
+        {
+            return Json(new
+            {
+                action = "Edit",
+                Id = id
+            });
+        }
+        public IActionResult Delete (int id)
+        {
+            return Json(new
+            {
+                action = "Delete",
+                Id = id
+            });
+        }
+    }
+}
